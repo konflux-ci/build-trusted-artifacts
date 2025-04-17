eaca69cb59ccafb762b90a3224795d32d5ec5185
Dheeraj <djodha@redhat.com>
2025-04-18 00:12:26 +0530

    Update retry.sh to include a note about the similar script in oras-container repo
    Co-authored-by: Simon Baird <simon.baird@gmail.com>

--- retry.sh
+++ retry.sh
@@ -2,6 +2,11 @@
 # Retry a command a few times if it fails
 # https://github.com/konflux-ci/build-trusted-artifacts/tree/main/retry.sh
 
+# Note: There is a similar retry script in
+# https://github.com/konflux-ci/oras-container/blob/main/hack/retry.sh
+# Potentially we could be using that shared base image here, and
+# consolidating the way the oras retries are done
+
 retry() {
   status=-1
   max_try=12
